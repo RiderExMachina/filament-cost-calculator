20a4fa02001c40863486583ea4f5ec01ce28ef9e
RiderExMachina <riderexmachina@protonmail.com>
2025-06-20 02:28:19 -0700

    good, working  beta release

---
+++ main.py
@@ -0,0 +1,102 @@
+import sys
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QLineEdit, QCheckBox, QPushButton, QVBoxLayout, QHBoxLayout, QWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Filament Cost Calculator")
+        widget = QWidget()
+
+        layout = QVBoxLayout()
+        filament = QHBoxLayout()
+        p_weight = QHBoxLayout()
+        p_time = QHBoxLayout()
+        options = QHBoxLayout()
+
+        self.banner = QLabel()
+        banner_font = self.banner.font()
+        banner_font.setPointSize(24)
+        self.banner.setFont(banner_font)
+        self.banner.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+
+        filament_cost_label = QLabel("Filament Cost per kg:")
+        self.filament_cost_entry = QComboBox()
+        self.filament_cost_entry.addItems({"17.99":[17.99], "13.99": [13.99]})
+        self.ppkilo = 17.99
+        self.filament_cost_entry.currentTextChanged.connect(self.filament_cost)
+        filament.addWidget(filament_cost_label)
+        filament.addWidget(self.filament_cost_entry)
+
+        print_weight_label = QLabel("Grams used in print:")
+        self.print_weight_entry = QLineEdit()
+        p_weight.addWidget(print_weight_label)
+        p_weight.addWidget(self.print_weight_entry)
+
+        print_time_label = QLabel("Time to print:")
+        self.print_time_entry = QLineEdit()
+        p_time.addWidget(print_time_label)
+        p_time.addWidget(self.print_time_entry)
+
+        on_time_label = QLabel("Printed at work/sleep")
+        self.on_time = QCheckBox()
+        options.addWidget(on_time_label)
+        options.addWidget(self.on_time)
+
+        calculate_button = QPushButton("Calculate")
+        calculate_button.clicked.connect(self.calculate_options)
+
+        layout.addLayout(filament)
+        layout.addLayout(p_weight)
+        layout.addLayout(p_time)
+        layout.addLayout(options)
+        layout.addWidget(self.banner)
+
+        layout.addWidget(calculate_button)
+
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def filament_cost(self, cost):
+        self.ppkilo = float(cost)
+        print(f"Set price: {cost} per kg ({ float(cost) / 1000 } per g)")
+
+    def calculate_options(self):
+        wtprint = int(self.print_weight_entry.text())
+        ttprint = self.print_time_entry.text()
+        if wtprint == "":
+            print("Needs Weight!")
+        if ttprint == "":
+            print("Needs Time!")
+
+        else:
+            if self.on_time.isChecked():
+                pphour = .5
+            if not self.on_time.isChecked():
+                pphour = 2
+            if "h" in ttprint:
+                hours = int(ttprint.split("h")[0]) * 60
+                minutes = int(ttprint.split("h")[1].replace("m", ""))
+                ttprint = hours + minutes
+            else:
+                ttprint = int(ttprint.replace("m", ""))
+
+            total_price = self.calc_price(pphour, ttprint, wtprint, self.ppkilo)
+            print(f"Print weighs {wtprint} g, will take {ttprint} minutes, and will cost {(float(self.ppkilo)/1000) * wtprint} to print")
+            self.banner.setText(f"Print will cost ${total_price}")
+    def calc_price(self, hour, time, weight, kilo):
+        print(f"{Qt.CheckState.Checked.value}")
+        hour_price = hour * time/60
+        print(f"Price per hour: {hour_price}")
+        weight_price = (float(kilo)/1000) * weight
+        print(f"Price for print: {weight_price}")
+        total_price = hour_price + weight_price
+
+        return round(total_price, 2)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    app.exec()
